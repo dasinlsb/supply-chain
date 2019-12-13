b938d403192b49438cc7e5c03c446e2e5ec6843c
dasinlsb <399359237@qq.com>
2019-12-14 00:19:58 +0800

    Remove temporary files in backend

--- backend/curltest.bash
+++
@@ -1,5 +0,0 @@
-curl -XPOST -H "Content-Type: application/json" -d '{"orgId": "1", "orgName": "sysu", "orgType": "enterprise", "corporationIdCardNumber": "33108", "phoneNumber": "133", "username": "dasin", "password":"123", "creatTime": "2019"}' localhost:8080/api/v1/users
-curl -XPOST -H "Content-Type: application/json" -d '{"username": "dasin", "password":"123"}' localhost:8080/api/v1/login
-
-
-cp /home/dasin/fisco/console/contracts/sdk/java/org/dasin/supply/contract/* /home/dasin/Courses/blkchain/supply/backend/src/main/java/org/dasin/supply/contract
